1a80ff1473d4e07c112997b49bf5d56342c58364
Jônatas Vitalino <jcv@cin.ufpe.br>
2020-09-22 23:41:25 -0300

    Adicionando o Navbar

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-alunos',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunosComponent implements OnInit {
 
+  titulo = 'Alunos';
+
+  public alunos = [
+    { id: 1, nome :'Marta', sobrenome: 'Luiza', telefone: 3212255},
+    { id: 2, nome :'Laura', sobrenome: 'Martins', telefone: 4332255},
+    { id: 3, nome :'Pedro', sobrenome: 'Suares', telefone: 6332255},
+    { id: 4, nome :'Luiza', sobrenome: 'Sergio', telefone: 9332255},
+    { id: 5, nome :'Lucas', sobrenome: 'Kaio', telefone: 2332255},
+    { id: 6, nome :'Paula', sobrenome: 'Costa', telefone: 1332255},
+    { id: 7, nome :'Paulo', sobrenome: 'Luiz', telefone: 8332255}
+     ];
   constructor() { }
 
   ngOnInit() {
--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessoresComponent implements OnInit {
 
+
+  public titulo = 'Professores';
+  public professores = [
+  {id: 1, nome :'Roberto' , disciplina: 'Matemática'},
+  {id: 2, nome :'Sérgio' , disciplina: 'Física'},
+  {id: 3, nome :'Junior' , disciplina: 'Português'},
+  {id: 4, nome :'Montana' , disciplina: 'Inglês'},
+  {id: 5, nome :'Tereza' , disciplina: 'Biologia'}
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
